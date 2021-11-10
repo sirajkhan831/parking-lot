e65e6e32545036989016f330fb1d0d3e10e05a28
Siraj <sirajkhanc2305@gmail.com>
2021-11-10 13:47:03 +0530

    Added: Documentations in the ParkingLot class

--- src/main/java/com/bridgelabz/ParkingLot.java
+++ src/main/java/com/bridgelabz/ParkingLot.java
@@ -1,8 +1,22 @@
 package com.bridgelabz;
 
+/**
+ * @author -> Siraj
+ * @version -> 1.0
+ * @since -> 05/11/2021
+ */
 public class ParkingLot {
+
+    /**
+     * This program manages parking spaces for vehicles.
+     */
     private Object vehicle;
 
+    /**
+     *
+     * @param vehicle -> Required to park the given vehicle.
+     * @return -> Returns boolean by checking if the given vehicle is already parked.
+     */
     public boolean park(Object vehicle) {
         if (this.vehicle != null) {
             return false;
@@ -11,6 +25,11 @@ public class ParkingLot {
         return true;
     }
 
+    /**
+     *
+     * @param vehicle -> Required to un-park the given vehicle.
+     * @return -> Returns boolean by checking if the given vehicle exists.
+     */
     public boolean unPark(Object vehicle) {
         if (vehicle == null) {
             return false;
