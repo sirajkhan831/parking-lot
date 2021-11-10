3f5ee1b276e32a5bb6892a223cc6f95340901bae
Siraj <sirajkhanc2305@gmail.com>
2021-11-10 13:38:07 +0530

    Added: TestCase for checking vehicle parking status

--- src/main/java/com/bridgelabz/ParkingLot.java
+++ src/main/java/com/bridgelabz/ParkingLot.java
@@ -1,5 +1,24 @@
 package com.bridgelabz;
 
 public class ParkingLot {
+    private Object vehicle;
 
+    public boolean park(Object vehicle) {
+        if (this.vehicle != null) {
+            return false;
+        }
+        this.vehicle = vehicle;
+        return true;
+    }
+
+    public boolean unPark(Object vehicle) {
+        if (vehicle == null) {
+            return false;
+        }
+        if (this.vehicle.equals(vehicle)) {
+            this.vehicle = null;
+            return true;
+        }
+        return false;
+    }
 }
--- src/test/java/com/bridgelabz/ParkingLotTest.java
+++ src/test/java/com/bridgelabz/ParkingLotTest.java
@@ -1,5 +1,29 @@
 package com.bridgelabz;
 
-public class ParkingLotTest {
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
+public class ParkingLotTest {
+    ParkingLot parkingLot = null;
+    Object vehicle = null;
+
+    @Before
+    public void setUp(){
+        vehicle = new Object();
+        parkingLot = new ParkingLot();
+    }
+
+    @Test
+    public void givenAVehicle_whenParked_ShouldReturnTrue() {
+        boolean isParked = parkingLot.park(new Object());
+        Assert.assertTrue(isParked);
+    }
+
+    @Test
+    public void givenAVehicle_whenUnParked_ShouldReturnTrue() {
+        parkingLot.park(vehicle);
+        boolean isUnParked = parkingLot.unPark(vehicle);
+        Assert.assertTrue(isUnParked);
+    }
 }
